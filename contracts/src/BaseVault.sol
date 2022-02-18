0244096c789b593cf5e004e5121f59c2abecb1a8
highstack-co <hello@highstack.co>
2022-02-18 16:47:14 -0700

    added details for nft generator

--- contracts/src/BaseVault.sol
+++ contracts/src/BaseVault.sol
@@ -6,7 +6,6 @@ import "./tokens/ERC20.sol";
 import "./interfaces/IStrategy.sol";
 
 abstract contract BaseVault is ERC721 {
-
     // #########################
     // ##                     ##
     // ##      Structs        ##
@@ -33,7 +32,7 @@ abstract contract BaseVault is ERC721 {
     uint256 SCALAR = 1e10;
 
     uint256 public depositedToStrat;
-    
+
     ERC20 immutable vaultToken;
     address immutable deployer; // can only set the strat ONCE
     IStrategy strat;
@@ -48,11 +47,11 @@ abstract contract BaseVault is ERC721 {
         ERC20 _vaultToken,
         string memory name,
         string memory symbol
+
     ) ERC721(name, symbol) {
 
         vaultToken = _vaultToken;
         deployer = msg.sender;
-
     }
 
     // #########################
@@ -65,7 +64,7 @@ abstract contract BaseVault is ERC721 {
         return _mintNewNFT(amount);
     }
 
-    function depositToId(uint256 amount, uint256 id) public virtual{
+    function depositToId(uint256 amount, uint256 id) public virtual {
         _depositToId(amount, id);
     }
 
@@ -74,26 +73,28 @@ abstract contract BaseVault is ERC721 {
     }
 
     // Burns NFT and withdraws all claimable token + yeild
-    function burnNFTAndWithdrawl(uint256 id) public virtual  {
-
+    function burnNFTAndWithdrawl(uint256 id) public virtual {
         uint256 claimable = withdrawableById(id);
         _withdrawFromId(claimable, id);
-        
-         // erc721
-        _burn(id);
 
+        // erc721
+        _burn(id);
     }
 
-    function withdrawableById(uint256 id) public view virtual returns (uint256) {
-
+    function withdrawableById(uint256 id)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
         uint256 yield = yieldPerId(id);
 
         // claimable may be larger than total deposits but never smaller
-        uint256 claimable = vaultToken.balanceOf(address(this)) + depositedToStrat;
+        uint256 claimable = vaultToken.balanceOf(address(this)) +
+            depositedToStrat;
         uint256 claimId = (claimable * deposits[id].amount) / totalDeposits;
 
         return claimId + yield;
-
     }
 
     // #########################
@@ -104,7 +105,6 @@ abstract contract BaseVault is ERC721 {
     // #########################
 
     function _mintNewNFT(uint256 amount) internal returns (uint256) {
-
         uint256 id = _mint(msg.sender, currentId);
 
         deposits[id].amount = amount;
@@ -115,11 +115,9 @@ abstract contract BaseVault is ERC721 {
         vaultToken.transferFrom(msg.sender, address(this), amount);
 
         return id;
-
     }
 
     function _depositToId(uint256 amount, uint256 id) internal {
-
         // trusted contract
         require(msg.sender == ownerOf[id]);
 
@@ -129,17 +127,15 @@ abstract contract BaseVault is ERC721 {
 
         //ensure token reverts on failed
         vaultToken.transferFrom(msg.sender, address(this), amount);
-        
     }
 
-    function _withdrawFromId(uint256 amount, uint256 id) internal  {
-
+    function _withdrawFromId(uint256 amount, uint256 id) internal {
         require(msg.sender == ownerOf[id]);
         require(amount <= withdrawableById(id));
 
         //trusted contract
         uint256 balanceCheck = vaultToken.balanceOf(address(this));
-        
+
         adjustYeild();
 
         uint256 userYield = yieldPerId(id);
@@ -150,18 +146,15 @@ abstract contract BaseVault is ERC721 {
 
         // trusted contract
         if (amount > balanceCheck) {
-            withdrawFromStrat(
-                amount - balanceCheck
-            );
+            withdrawFromStrat(amount - balanceCheck);
 
             depositedToStrat -= adjusted;
         }
-        
+
         deposits[id].amount -= adjusted;
         deposits[id].tracker -= amount * yeildPerDeposit;
 
         vaultToken.transfer(msg.sender, amount);
-
     }
 
     // #########################
@@ -172,7 +165,6 @@ abstract contract BaseVault is ERC721 {
 
     //total possible deposited to strat is currently set at 50%
     function initStrat() public {
-
         require(address(strat) != address(0), "No Strategy");
 
         // 50% of total deposits
@@ -180,18 +172,15 @@ abstract contract BaseVault is ERC721 {
         uint256 depositable = half - depositedToStrat;
 
         depositedToStrat += depositable;
-        
+
         vaultToken.approve(address(strat), depositable);
         strat.deposit(depositable);
-
     }
 
     //internal, only called when balanceOf(address(this)) < withdraw requested
     // depositedToStrat and totalDeposits = total withdrawn - yeild of msg.sender
     function withdrawFromStrat(uint256 amountNeeded) internal {
-
         strat.withdrawl(amountNeeded);
-
     }
 
     // #########################
@@ -203,21 +192,17 @@ abstract contract BaseVault is ERC721 {
     // gets yeild from strategy contract
     //possbily call this before new mints?
     function adjustYeild() public virtual {
-
         require(address(strat) != address(0), "No Strategy");
 
         uint256 totalInStrat = strat.withdrawlableVaultToken();
         uint256 totalYield = totalInStrat - depositedToStrat;
 
         yeildPerDeposit += (totalYield * SCALAR) / totalDeposits;
-
     }
 
     function yieldPerId(uint256 id) public view returns (uint256) {
-
-        uint256 pre = deposits[id].amount * yeildPerDeposit / SCALAR;
+        uint256 pre = (deposits[id].amount * yeildPerDeposit) / SCALAR;
         return pre - deposits[id].tracker / SCALAR;
-
     }
 
     // #########################
@@ -227,6 +212,7 @@ abstract contract BaseVault is ERC721 {
     // #########################
 
     function tokenURI(uint256 id) public view override returns (string memory) {
+        return INftDataGenerator(nftDataGenerator).generateTokenUri(this, id);
         return "string";
     }
 
@@ -241,6 +227,5 @@ abstract contract BaseVault is ERC721 {
         require ( msg.sender == deployer && address(strat) == address(0) );
 
         strat = IStrategy(addr);
-
     }
 }
