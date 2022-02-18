2094224171a234acc01d0f2d09828e35d7a3c9ce
highstack-co <89357343+highstack-co@users.noreply.github.com>
2022-02-18 15:53:39 -0800

    added details for nft generator (#3)

--- contracts/src/BaseVault.sol
+++ contracts/src/BaseVault.sol
@@ -5,6 +5,7 @@ import "./tokens/ERC721.sol";
 import "./tokens/ERC20.sol";
 import "./interfaces/IStrategy.sol";
 
+
 contract BaseVault is ERC721 {
 
     // #########################
@@ -33,7 +34,7 @@ contract BaseVault is ERC721 {
     uint256 SCALAR = 1e10;
 
     uint256 public depositedToStrat;
-    
+
     ERC20 immutable vaultToken;
     address immutable deployer; // can only set the strat ONCE
     IStrategy strat;
@@ -48,11 +49,11 @@ contract BaseVault is ERC721 {
         ERC20 _vaultToken,
         string memory name,
         string memory symbol
+
     ) ERC721(name, symbol) {
 
         vaultToken = _vaultToken;
         deployer = msg.sender;
-
     }
 
     // #########################
@@ -65,7 +66,7 @@ contract BaseVault is ERC721 {
         return _mintNewNFT(amount);
     }
 
-    function depositToId(uint256 amount, uint256 id) public virtual{
+    function depositToId(uint256 amount, uint256 id) public virtual {
         _depositToId(amount, id);
     }
 
@@ -74,26 +75,28 @@ contract BaseVault is ERC721 {
     }
 
     // Burns NFT and withdraws all claimable token + yeild
-    function burnNFTAndWithdrawl(uint256 id) public virtual  {
-
+    function burnNFTAndWithdrawl(uint256 id) public virtual {
         uint256 claimable = withdrawableById(id);
         _withdrawFromId(claimable, id);
-        
-         // erc721
-        _burn(id);
 
+        // erc721
+        _burn(id);
     }
 
-    function withdrawableById(uint256 id) public view virtual returns (uint256) {
-
+    function withdrawableById(uint256 id)
+        public
+        view
+        virtual
+        returns (uint256)
+    {
         uint256 yield = yieldPerId(id);
 
         // claimable may be larger than total deposits but never smaller
-        uint256 claimable = vaultToken.balanceOf(address(this)) + depositedToStrat;
+        uint256 claimable = vaultToken.balanceOf(address(this)) +
+            depositedToStrat;
         uint256 claimId = (claimable * deposits[id].amount) / totalDeposits;
 
         return claimId + yield;
-
     }
 
     // #########################
@@ -104,7 +107,6 @@ contract BaseVault is ERC721 {
     // #########################
 
     function _mintNewNFT(uint256 amount) internal returns (uint256) {
-
         uint256 id = _mint(msg.sender, currentId);
 
         deposits[id].amount = amount;
@@ -115,11 +117,9 @@ contract BaseVault is ERC721 {
         vaultToken.transferFrom(msg.sender, address(this), amount);
 
         return id;
-
     }
 
     function _depositToId(uint256 amount, uint256 id) internal {
-
         // trusted contract
         require(msg.sender == ownerOf[id]);
 
@@ -129,11 +129,9 @@ contract BaseVault is ERC721 {
 
         //ensure token reverts on failed
         vaultToken.transferFrom(msg.sender, address(this), amount);
-        
     }
 
-    function _withdrawFromId(uint256 amount, uint256 id) internal  {
-
+    function _withdrawFromId(uint256 amount, uint256 id) internal {
         require(msg.sender == ownerOf[id]);
         require(amount <= withdrawableById(id));
         
@@ -167,7 +165,6 @@ contract BaseVault is ERC721 {
         deposits[id].amount -= adjusted;
 
         vaultToken.transfer(msg.sender, amount);
-
     }
 
     // #########################
@@ -178,7 +175,6 @@ contract BaseVault is ERC721 {
 
     //total possible deposited to strat is currently set at 50%
     function initStrat() public {
-
         require(address(strat) != address(0), "No Strategy");
 
         // 50% of total deposits
@@ -186,18 +182,15 @@ contract BaseVault is ERC721 {
         uint256 depositable = half - depositedToStrat;
 
         depositedToStrat += depositable;
-        
+
         vaultToken.approve(address(strat), depositable);
         strat.deposit(depositable);
-
     }
 
     //internal, only called when balanceOf(address(this)) < withdraw requested
     // depositedToStrat and totalDeposits = total withdrawn - yeild of msg.sender
     function withdrawFromStrat(uint256 amountNeeded) internal {
-
         strat.withdrawl(amountNeeded);
-
     }
 
     // #########################
@@ -209,21 +202,17 @@ contract BaseVault is ERC721 {
     // gets yeild from strategy contract
     //possbily call this before new mints?
     function adjustYeild() public virtual {
-
         require(address(strat) != address(0), "No Strategy");
 
         uint256 totalInStrat = strat.withdrawlableVaultToken();
         uint256 totalYield = totalInStrat - depositedToStrat;
 
         yeildPerDeposit += (totalYield * SCALAR) / totalDeposits;
-
     }
 
     function yieldPerId(uint256 id) public view returns (uint256) {
-
-        uint256 pre = deposits[id].amount * yeildPerDeposit / SCALAR;
+        uint256 pre = (deposits[id].amount * yeildPerDeposit) / SCALAR;
         return pre - deposits[id].tracker / SCALAR;
-
     }
 
     // #########################
@@ -233,6 +222,7 @@ contract BaseVault is ERC721 {
     // #########################
 
     function tokenURI(uint256 id) public view override returns (string memory) {
+        return INftDataGenerator(nftDataGenerator).generateTokenUri(this, id);
         return "string";
     }
 
@@ -247,6 +237,5 @@ contract BaseVault is ERC721 {
         require ( msg.sender == deployer && address(strat) == address(0) );
 
         strat = IStrategy(addr);
-
     }
 }
--- contracts/src/VaultFactory.sol
+++ contracts/src/VaultFactory.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.8.0;
 
 import "./tokens/ERC721.sol";
-import "./DegenVault.sol";
 import "./interfaces/IStrategy.sol";
 
 interface iVault {
@@ -24,6 +23,8 @@ contract VaultFactory {
     uint256 public vaultsLength;
     mapping (uint256 => bytes) public stratType;
     uint256 public stratsLength;
+    mapping (uint256 => bytes) public nftGeneratorType;
+    uint256 public nftGeneratorsLength;
 
     address[] public vaults;
 
@@ -34,23 +35,27 @@ contract VaultFactory {
         uint256 _stratType,
         address vaultToken, 
         address yield,
-        bytes calldata _constructor
+        bytes calldata _constructor,
+        bytes calldata _nftGeneratorConstructor
 
     ) public returns(address vault) {
 
         bytes memory newVault = abi.encodePacked(vaultType[_vaultKey], _constructor);
         bytes memory newStrat = abi.encodePacked(stratType[_stratType], abi.encode(yield, vaultToken));
+        bytes memory newNftGenerator = abi.encodePacked(nftGeneratorType[_vaultKey], _nftGeneratorConstructor);
         address strat;
+        address nftGenerator;
 
         // use create2
         assembly {
             strat := create(0, add(newStrat, 0x20), mload(newStrat))
             vault := create(0, add(newVault, 0x20), mload(newVault))
+            nftGenerator := create(0, add(newNftGenerator, 0x20), mload(newNftGenerator))
         }
 
         iVault(vault).setStrat(strat);
         IStrategy(strat).initVault(vault);
-
+        // iNftDataGenerator(nftGenerator);
     }
 
     function addVault(bytes calldata newVault) external returns (uint256) {
@@ -59,6 +64,16 @@ contract VaultFactory {
         uint256 current = vaultsLength;
         vaultType[vaultsLength] = newVault;
         vaultsLength++;
+
+        return current;
+    }
+
+    function addNftGenerator(bytes calldata newNftGenerator) external returns (uint256) {
+        require (msg.sender == creator);
+
+        uint256 current = nftGeneratorsLength;
+        nftGeneratorType[nftGeneratorsLength] = newNftGenerator;
+        nftGeneratorsLength++;
 
         return current;
     }
