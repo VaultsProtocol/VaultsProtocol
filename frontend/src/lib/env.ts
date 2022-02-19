f27b275ac6cac62004a2ac15e120e3ded7897152
Darryl Yeo <dev@darryl-yeo.com>
2022-02-18 18:24:14 -0800

    env defaults

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -1,9 +1,9 @@
 export const env = {
-	NETWORK_ID: Number(import.meta.env.VITE_NETWORK_ID),
-	ETHEREUM_NODE_URI: String(import.meta.env.VITE_ETHEREUM_NODE_URI),
-	ETHERSCAN_BASE_URI: String(import.meta.env.VITE_ETHERSCAN_BASE_URI),
-	WALLET_CONNECT_BRIDGE_URI: String(import.meta.env.VITE_WALLET_CONNECT_BRIDGE_URI),
-	NFT_CONTRACT_ADDRESS: String(import.meta.env.VITE_NFT_CONTRACT_ADDRESS),
+	NETWORK_ID: Number(import.meta.env.VITE_NETWORK_ID || 1),
+	ETHEREUM_NODE_URI: String(import.meta.env.VITE_ETHEREUM_NODE_URI || ''),
+	ETHERSCAN_BASE_URI: String(import.meta.env.VITE_ETHERSCAN_BASE_URI || ''),
+	WALLET_CONNECT_BRIDGE_URI: String(import.meta.env.VITE_WALLET_CONNECT_BRIDGE_URI || ''),
+	NFT_CONTRACT_ADDRESS: String(import.meta.env.VITE_NFT_CONTRACT_ADDRESS || ''),
 
 	ALCHEMY_API_KEY_MAINNET: '',
 	ETHERSCAN_API_KEY: '',
