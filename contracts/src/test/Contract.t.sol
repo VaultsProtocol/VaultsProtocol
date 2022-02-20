706eb8ec1107a72ff06177ada6c5a4a2f724bc6d
highstack-co <hello@highstack.co>
2022-02-19 20:28:23 -0700

    added deploy script rpcs

--- contracts/src/test/Contract.t.sol
+++ contracts/src/test/Contract.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity >=0.8.0;
 
-import "ds-test/test.sol";
+import "../../lib/ds-test/src/test.sol";
 import "../BaseVault.sol";
 import "../tokens/MockERC20.sol";
 import {console} from "./Console.sol";
