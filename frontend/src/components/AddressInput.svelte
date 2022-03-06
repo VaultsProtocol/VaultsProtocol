2a2ac57b2dcee62076368d709eb4cc1c7cb51484
Darryl Yeo <dev@darryl-yeo.com>
2022-03-06 09:28:13 -0800

    Don't update bound value if invalid

--- frontend/src/components/AddressInput.svelte
+++ frontend/src/components/AddressInput.svelte
@@ -4,14 +4,21 @@
 
 
 	// Internal state
+	let _address: string = address
 	let isValid: boolean
-	$: isValid = isAddress(address)
-
 	let isResolvingEns = false
 
 
 	// Methods/hooks/lifecycle
-	isResolvingEns
+	// $: _address = $$props.address
+	$: isValid = !_address || isAddress(_address)
+
+	const onChange = () => {
+		// if(isValid)
+		// 	address = _address
+	}
+	$: if(isValid)
+		address = _address
 
 
 	// Components
@@ -21,15 +28,16 @@
 
 <input
 	type="text"
-	bind:value={address}
-	class:invalid={address && !isValid}
+	bind:value={_address}
+	class:invalid={!isValid}
 	placeholder="0xabc...6789 / ens.eth"
-	on:blur={() => address = address.trim()}
+	on:input={onChange}
+	on:blur={() => _address = _address.trim()}
 />
 
 
 <style>
-	.invalid {
+	:not(:placeholder-shown).invalid {
 		border: red solid 2px;
 	}
 </style>
