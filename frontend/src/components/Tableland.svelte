c17c794027c3910bf50466f59f0f58b4a9925d7f
Darryl Yeo <dev@darryl-yeo.com>
2022-03-18 19:52:45 -0700

    Add Tableland icon; tweak layout

--- frontend/src/components/Tableland.svelte
+++ frontend/src/components/Tableland.svelte
@@ -37,25 +37,33 @@
 	import Container from './Container.svelte'
 
 
+	// Images
+	import tablelandIcon from '../assets/dapps/tableland.png'
+
+
 	// Styles/transitions
-	import { fly, scale } from 'svelte/transition'
+	import { fly, scale, fade } from 'svelte/transition'
 </script>
 
 
 <article class="card column">
 	<header class="row">
-		<slot name="title"
-			{connection}
-			{network} {account}
-		/>
+		<div class="row-inline">
+			<img src={tablelandIcon} width="30" />
+
+			<slot name="title"
+				{connection}
+				{network} {account}
+			/>
+		</div>
 
 		<div class="stack align-end">
 			{#if !account}
 				{$_('Connect wallet')}
 			{:else if !connection}
-				<button class="primary" on:click={connect} transition:scale>{$_('Sign in to Tableland')}</button>
+				<button class="primary large" on:click={connect} transition:scale>{$_('Sign in to Tableland')}</button>
 			{:else}
-				<button class="destructive" on:click={disconnect} transition:scale>{$_('Disconnect')}</button>
+				<button class="destructive large" on:click={disconnect} transition:scale>{$_('Disconnect')}</button>
 			{/if}
 		</div>
 	</header>
@@ -63,11 +71,13 @@
 	<Container isOpen={!!connection} renderOnlyWhenOpen>
 		<main class="stack">
 			{#await getTables(connection)}
-				<div class="card column centered" transition:scale>
+				<div class="card row centered" transition:scale>
+					<img src={tablelandIcon} width="30" />
 					{$_('Fetching your tables from Tableland...')}
 				</div>
 			{:then tables}
-				<div class="column list" transition:scale>
+				<div class="column list">
+					<hr transition:scale />
 					{#each tables as table, i (table.name)}
 						<article class="card row" transition:fly={{ x: 100, delay: i * 200 }}>
 							<slot name="table"
