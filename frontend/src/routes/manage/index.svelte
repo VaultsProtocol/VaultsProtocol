56635cd0db8be26b1affe7fee354e523e184c2ed
Darryl Yeo <dev@darryl-yeo.com>
2022-03-20 01:02:13 -0700

    Load deployed vaults from local storage, vault positions

--- frontend/src/routes/manage/index.svelte
+++ frontend/src/routes/manage/index.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	// Constants/types
 	import { _ } from 'svelte-i18n'
+	import { networksByChainID } from '$lib/networks'
 
 
 	// Stores
 	import { account } from '../../stores/account'
+	import { rpcProvider } from '../../stores/rpcProvider'
+
+	import { deployedVaults } from '../../stores/deployedVaults'
 
 
 	// Formatting
@@ -14,41 +18,43 @@
 
 	// Methods/hooks/lifecycle
 
-	import { getRandomVaultConfig } from '../../lib/vaults'
+	import { getVaultStatus, getVaultPosition } from '$lib/vaultsProtocol'
 
-	const getPlaceholderVault = () => {
-		const vaultConfig = getRandomVaultConfig()
+	// import { getRandomVaultConfig } from '../../lib/vaults'
 
-		const { decimals } = vaultConfig.tokens[0] ?? {}
+	// const getPlaceholderVault = () => {
+	// 	const vaultConfig = getRandomVaultConfig()
 
-		return {
-			vaultConfig,
+	// 	const { decimals } = vaultConfig.tokens[0] ?? {}
 
-			vaultStatus: {
-				contractAddress: '0x0000000000000000000000000000000000000000',
-				tokenId: 0,
-				totalBalance: parseUnits((Math.random() * 12345678 | 0).toString(), decimals),
-				endTimestamp: Date.now() + 120000
-			},
+	// 	return {
+	// 		vaultConfig,
 
-			vaultPosition: {
-				balance: parseUnits('1234', decimals),
-				yieldEarned: parseUnits('1000', decimals)
-			}
-		}
-	}
+	// 		vaultStatus: {
+	// 			contractAddress: '0x0000000000000000000000000000000000000000',
+	// 			tokenId: 0,
+	// 			totalBalance: parseUnits((Math.random() * 12345678 | 0).toString(), decimals),
+	// 			endTimestamp: Date.now() + 120000
+	// 		},
 
-	const vaults = [
-		getPlaceholderVault(),
-		getPlaceholderVault(),
-		getPlaceholderVault(),
-		getPlaceholderVault(),
-		getPlaceholderVault(),
-		getPlaceholderVault(),
-		getPlaceholderVault(),
-		getPlaceholderVault(),
-		getPlaceholderVault(),
-	]
+	// 		vaultPosition: {
+	// 			balance: parseUnits('1234', decimals),
+	// 			yieldEarned: parseUnits('1000', decimals)
+	// 		}
+	// 	}
+	// }
+
+	// const vaults = [
+	// 	getPlaceholderVault(),
+	// 	getPlaceholderVault(),
+	// 	getPlaceholderVault(),
+	// 	getPlaceholderVault(),
+	// 	getPlaceholderVault(),
+	// 	getPlaceholderVault(),
+	// 	getPlaceholderVault(),
+	// 	getPlaceholderVault(),
+	// 	getPlaceholderVault(),
+	// ]
 
 
 	// Internal state
@@ -60,41 +66,86 @@
 	// Components
 	import TablelandVaults from '../../components/TablelandVaults.svelte'
 	import Vault from '../../components/Vault.svelte'
-	import VaultManager from '../../components/VaultManager.svelte'
+	import VaultPositionManager from '../../components/VaultPositionManager.svelte'
+
 
 	// Images
 	import manageIcon from '../../assets/icons/manage-icon.svg'
-
+	
+	
+	// Styles/animations
+	import { scale } from 'svelte/transition'
 </script>
 
-<section>
-	<h1>
-		<!-- temp -->
-		<img src={manageIcon} width="30" />
-		{$_('Manage Vaults')}</h1>
+<section class="column centered">
+	<h1 class="row-inline">
+		<img src={manageIcon} width="50" />
+		{$_('Manage Vaults')}
+	</h1>
 </section>
 
+<!-- <section class="row equal"> -->
 <section class="column">
+	<section class="card column">
+		<header class="row">
+			<h2>{$_('Your Deployed Vaults')}</h2>
+		</header>
+
+		{#each $deployedVaults as { vaultConfig, contractAddress }}
+			<div class="stack">
+				{#await (async () =>
+					({
+						vaultStatus: await getVaultStatus({
+							contractAddress,
+							network: networksByChainID[vaultConfig.chainId],
+							provider: $rpcProvider,
+						}),
+						vaultPositions: await Promise.all(
+							[0].map(tokenId =>
+								getVaultPosition({
+									contractAddress,
+									network: networksByChainID[vaultConfig.chainId],
+									provider: $rpcProvider,
+									tokenId,
+								})
+							)
+						)
+					})
+				)()}
+					<div class="card" transition:scale>
+						{$_('Loading your vault positions...')}
+					</div>
+				{:then { vaultStatus, vaultPositions }}
+					<div class="card" transition:scale>
+						{#each vaultPositions as vaultPosition}
+							<div class="manage-vault row">
+								<Vault
+									{vaultConfig}
+									{vaultStatus}
+									{vaultPosition}
+								/>
+
+								<VaultPositionManager
+									{vaultConfig}
+									{vaultStatus}
+									{vaultPosition}
+								/>
+							</div>
+						{/each}
+					</div>
+				{:catch e}
+					<div class="card" transition:scale>
+						<output>{e}</output>
+					</div>
+				{/await}
+			</div>
+		{/each}
+	</section>
+
 	<TablelandVaults
 		account={$account}
 	/>
 </section>
-
-{#each vaults as { vaultConfig, vaultStatus, vaultPosition }}
-	<section class="manage-vault row">
-		<Vault
-			{vaultConfig}
-			{vaultStatus}
-			{vaultPosition}
-		/>
-
-		<VaultManager
-			{vaultConfig}
-			{vaultStatus}
-			{vaultPosition}
-		/>
-	</section>
-{/each}
 
 
 <style>
