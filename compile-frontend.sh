11739f4305cca738079e684e12bf7ec9ab990b8c
Darryl Yeo <dev@darryl-yeo.com>
2022-03-20 15:23:44 -0700

    Compile contracts/types

--- compile-frontend.sh
+++ compile-frontend.sh
@@ -1,7 +1,8 @@
 rm -rf frontend/src/lib/contracts/
 mkdir frontend/src/lib/contracts
 find contracts/artifacts -name "*.json" ! -name "*.dbg.json" -exec cp -v {} frontend/src/lib/contracts \;
-find contracts/typechain-types -type f -exec cp -v {} frontend/src/lib/contracts \;
+cp -r contracts/typechain-types/* frontend/src/lib/contracts
 
 # cp -r contracts/typechain-types/* contracts/artifacts/**/*.json frontend/src/lib/contracts
 # rm -r frontend/src/lib/contracts/**/*.dbg.json
+# find contracts/typechain-types -type f -exec cp -v {} frontend/src/lib/contracts \;
